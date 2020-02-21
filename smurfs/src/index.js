775e0c03e953cfcdc905d2d6b48f674ce439886e
Michelle Cupp <shellcupp@gmail.com>
2020-02-21 11:28:21 -0600

    got npm working

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -6,7 +6,6 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 
-
 const store = createStore( applyMiddleware(thunk));
 
 ReactDOM.render(
